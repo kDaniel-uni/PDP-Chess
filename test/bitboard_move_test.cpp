b4afd607645ef46bb98c43cf09edcf35587262de
Florian Salomode <florian.salomode@etu.u-bordeaux.fr>
2022-03-22 18:11:20 +0100

    First add of one implementation of move, need to verification

---
+++ test/bitboard_move_test.cpp
@@ -0,0 +1,23 @@
+//
+// Pdp_echec university project
+//
+
+#include <string.h>
+#include "../include/bitboard_operations.h"
+#include "../include/move.h"
+
+int main (int argc, char *argv[]) {
+
+
+  
+    struct move move_to_test;
+    move_to_test.moving_position = "h2";
+    move_to_test.target_position = "h4";
+    Bitboards* bitboards = new Bitboards(false);
+    bitboards = io_bitboard::traduct_from_move(bitboards,move_to_test);
+    if (io_bitboard::to_string(bitboards)== "rnbkqbnrpppppppp----------------P----------------PPPPPPPRNBQKBNR"){
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+
+}
