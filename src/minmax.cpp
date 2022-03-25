c1daf5757a7ea510fc4311bb843d9fbdf8787c56
Marvin Beites <marvin.beites@etu.u-bordeaux.fr>
2022-03-25 15:27:47 +0100

    add structure of algorithm minmax

---
+++ src/minmax.cpp
@@ -0,0 +1,43 @@
+//
+// Pdp_echec university project
+//
+
+#include "board.h"
+#include "move.h"
+#include "ai_player.h"
+
+using namespace std;
+
+int minmax(board board, int depth, ai_player ai_player){
+    int value = 0;
+    if(depth==0 || board.is_game_over()){
+        //return valeurPlateau(board);
+        return 0;
+    }
+    if(ai_player){
+        int value_max = INT_MIN;
+        move legal_moves = board.generate_legal_moves();
+        for(move move : legal_moves){
+            board.push(moves);
+            value = minmax(board, depth-1, false);
+            board.pop();
+            if value > value_max{
+                value_max = valeur;
+            }
+            return value;
+        }
+    }
+    else{
+            int value_min = INT_MAX;
+            move legal_moves = board.generate_legal_moves();
+            for(move move : legal_moves){
+                board.push(moves);
+                value = minmax(board,depth-1,true);
+                board.pop();
+                if value < value_min{
+                    value_min = valeur;
+                }
+                return value;
+            }
+        }
+}
