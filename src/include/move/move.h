fdb97cf23596745390475364d4ff47c55f301b81
Karl Daniel <karl.daniel@etu.u-bordeaux.fr>
2022-03-31 16:11:13 +0200

    add types in move struct

--- src/include/move/move.h
+++ src/include/move/move.h
@@ -5,11 +5,15 @@
 #ifndef PDP_CHESS_MOVE_H
 #define PDP_CHESS_MOVE_H
 
+#include "bitboards.h"
+
 namespace pdp_chess {
 
     struct Move{
         uint8_t start_position;
+        char start_type;
         uint8_t target_position;
+        char target_type;
     };
 
 }
