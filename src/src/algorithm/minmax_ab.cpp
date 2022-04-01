19790ea1403db15624200ec40e40ebb5fa85335e
Florian Salomode <florian.salomode@etu.u-bordeaux.fr>
2022-04-01 16:00:51 +0200

    update and add algorithm struct and little fix minmax ab (call good board functions)

--- src/src/algorithm/minmax_ab.cpp
+++ src/src/algorithm/minmax_ab.cpp
@@ -5,53 +5,69 @@
 #include "board.h"
 #include "move.h"
 #include "ai_player.h"
+#include "legal_move.h"
 #include <algorithm>
 #include <limits> 
 
 namespace pdp_chess {
+    class MinMax_ab : public AiPlayer{
 
-    int minmax_alphabeta(Board board, int depth, int alpha, int beta, AiPlayer ai_player){
-        int value = 0;
-        if(depth==0 || board.is_game_over()){
-            //return ValeurPlateau(board);
-            return 0;
+
+
+        Move findNextMove(Board& board, color current_color){
+            
         }
-        if(ai_player){
-            int value_max = std::numeric_limits<int>::min();
-            move[] legal_moves = board.generate_legal_moves();
-            for (move move : legal_moves)
-            {
-                board.push(move);
-                value = minmax_alphabeta(board, depth-1, alpha, beta, False);
-                board.pop();
-                if(value > value_max){
-                    value_max = value;
+
+        std::string getParameters(){
+
+        }
+
+
+
+
+        int minmax_alphabeta(Board board, int depth, int alpha, int beta, bool ai_player_turn){
+            int value = 0;
+            if(depth==0 || board.isGameOver()){
+                //return ValeurPlateau(board);
+                return 0;
+            }
+            if(ai_player_turn){
+                int value_max = std::numeric_limits<int>::min();
+                std::vector<Move> legal_moves = legal_move(board,ai_player_turn);
+                for (Move move : legal_moves)
+                {
+                    board.doMove(move);
+                    value = minmax_alphabeta(board, depth-1, alpha, beta, !ai_player_turn);
+                    board.undoMove();
+                    if(value > value_max){
+                        value_max = value;
+                    }
+                    alpha = std::max(alpha, value);
+                    if(beta<=alpha){
+                        break;
+                    }
+                    return value;
                 }
-                alpha = std::max(alpha, value);
-                if(beta<=alpha){
-                    break;
+            }
+            else{
+                int value_min = std::numeric_limits<int>::max();
+                std::vector<Move> legal_moves = legal_move(board,ai_player_turn);
+                for (Move move : legal_moves)
+                {
+                    board.doMove(move);
+                    value = minmax_alphabeta(board, depth-1, alpha, beta, !ai_player_turn);
+                    board.undoMove();
+                    if(value < value_min){
+                        value_min = value;
+                    }
+                    beta = std::min({beta, value});
+                    if(beta<=alpha){
+                        break;
+                    }
+                    return value;
                 }
-                return value;
             }
         }
-        else{
-            int value_min = std::numeric_limits<int>::max();
-            move[] legal_moves = board.generate_legal_moves();
-            for (move move : legal_moves)
-            {
-                board.push(move);
-                value = minmax_alphabeta(board, depth-1, alpha, beta, True);
-                board.pop();
-                if(value < value_min){
-                    value_min = value;
-                }
-                beta = std::min({beta, value});
-                if(beta<=alpha){
-                    break;
-                }
-                return value;
-            }
-        }
-    }
+    };
 
 }
---
+++ src/src/algorithm/negamax.cpp
@@ -0,0 +1,20 @@
+#include "board.h"
+#include "move.h"
+#include "ai_player.h"
+#include <algorithm>
+#include <limits> 
+
+namespace pdp_chess {
+    class NegaMax : public AiPlayer{
+
+
+        Move findNextMove(Board& board, color current_color){
+            
+        }
+
+        std::string getParameters(){
+
+        }
+    };
+
+}
