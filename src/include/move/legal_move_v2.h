63851d70dcc80202ba7986e3628d4fb90ae5b0f9
Florian Salomode <florian.salomode@etu.u-bordeaux.fr>
2022-04-01 17:41:19 +0200

    fix not_a and h border for legal move

--- src/include/move/legal_move_v2.h
+++ src/include/move/legal_move_v2.h
@@ -9,8 +9,8 @@
 
 namespace pdp_chess {
 uint64_t base = 1;
-uint64_t const not_a_border = (base >> 0) + (base >> 8) + (base >> 16) + (base >> 24) + (base >> 32) + (base >> 40) + (base >> 48) + (base >> 58);
-uint64_t const not_h_border = (base >> 7) + (base >> 15) + (base >> 23) + (base >> 31) + (base >> 39) + (base >> 47) + (base >> 57) + (base >> 63);
+uint64_t const not_a_border = 18374403900871474942ULL;
+uint64_t const not_h_border = 9187201950435737471ULL;
 
 uint64_t pawns_attack_table[2][64];
 uint64_t pawnsAttacks(int color, int position); // color {white = 0 ; black = 1}
