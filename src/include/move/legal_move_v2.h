8bbb4f784c0a7fc3b2e1ef93ce12e650508db565
Florian Salomode <florian.salomode@etu.u-bordeaux.fr>
2022-04-02 15:39:18 +0200

    change struct of legal_move_v2 and add print bitboard fonction for debug

--- src/include/move/legal_move_v2.h
+++ src/include/move/legal_move_v2.h
@@ -1,20 +1,27 @@
 #ifndef PDP_CHESS_LEGAL_MOVE_V2_H
 #define PDP_CHESS_LEGAL_MOVE_V2_H
 
-#include "game.h"
+
+
+#include <cstdint>
 #include "bitboards.h"
-#include "string"
-#include "vector"
+#include <iostream>
+#include <vector>
+#include "board.h"
 #include "move.h"
 
 namespace pdp_chess {
-uint64_t base = 1;
-uint64_t const not_a_border = 18374403900871474942ULL;
-uint64_t const not_h_border = 9187201950435737471ULL;
+    uint64_t const not_a_border = 18374403900871474942ULL;
+    uint64_t const not_h_border = 9187201950435737471ULL;
 
-uint64_t pawns_attack_table[2][64];
-uint64_t pawnsAttacks(int color, int position); // color {white = 0 ; black = 1}
-void initPawnsAttacksTable();
+
+    std::vector<Move> legalMove(const Board& board, bool white);
+
+    uint64_t pawnsAttacks(int color, int position); // color {white = 0 ; black = 1}
+
+    std::string bitboardToString(uint64_t mask);// for a debug usage
+    void printBitboard(uint64_t mask); // for a debug usage
+
 
 }
 
--- src/src/move/legal_move_v2.cpp
+++ src/src/move/legal_move_v2.cpp
@@ -1,45 +1,76 @@
-#include "game.h"
+
 #include "legal_move_v2.h"
-#include "bitboard_operations.h"
+
 
 namespace pdp_chess {
 
 
-uint64_t pawnsAttacks(int color, int position){
+    uint64_t pawnsAttacks(int color, int position){
+        uint64_t b;
+        uint64_t bitboard = (b >> position);
+        uint64_t mask = 0;
 
-    uint64_t bitboard = (base >> position);
-    uint64_t mask = 0;
+        if(color){
+            if((bitboard >> 7) & not_a_border){
+                mask |= (bitboard >> 7);
+            }
+            if((bitboard >> 9) & not_h_border){
+                mask |= (bitboard >> 9);
+            }
+        }
 
-    if(color){
-        if((bitboard >> 7) & not_a_border){
-            mask |= (bitboard >> 7);
+        else{
+            if((bitboard << 7) & not_h_border){
+                mask |= (bitboard << 7);
+            }
+            if((bitboard << 9) & not_a_border){
+                mask |= (bitboard << 9);
+            }
         }
-        if((bitboard >> 9) & not_h_border){
-            mask |= (bitboard >> 9);
-        }
+
+        return mask;
     }
 
-    else{
-        if((bitboard << 7) & not_h_border){
-            mask |= (bitboard << 7);
+    std::string bitboardToString(uint64_t mask){
+        std::string res;
+        res.resize(64);
+        int dec = BOARD_SIZE-1;
+
+        for(int i = 0; i<BOARD_SIZE; i++){
+            if((mask >> i)&1){
+                res[dec] = '1';
+            }
+            else{
+                res[dec] = '0';
+            }
+            dec--;
         }
-        if((bitboard << 9) & not_a_border){
-            mask |= (bitboard << 9);
-        }
+        
+        return res + std::to_string(mask);
     }
 
-    return mask;
-}
-
-void initPawnsAttacksTable(){
-    
-    for(int position = 0; position < 64; position ++){
-        pawns_attack_table[white][position] = pawnsAttacks(white,position);
-        pawns_attack_table[black][position] = pawnsAttacks(black,position);
+    void printBitboard(uint64_t mask){
+        std::string chars = bitboardToString(mask);
+        for (int current_y = 0; current_y <= 7; current_y++){
+                for (int current_x = 7; current_x >= 0; current_x--){
+                    std::cout << chars[current_y * 8 + current_x] << ' ';
+                }
+                std::cout << std::endl;
+            }
+        std::cout<< "Valeur totale du bitboard : "<< std::endl;
+        std::cout << chars.substr(64) << std::endl;
     }
-}
 
-
+    std::vector<Move> legalMove(const Board& board, bool white){
+        // init lookup table attacks
+        uint64_t pawns_attack_table[2][64];
+        for(int position = 0; position < 64; position ++){
+            pawns_attack_table[white][position] = pawnsAttacks(white,position);
+            pawns_attack_table[black][position] = pawnsAttacks(black,position);
+        }
+        std::vector<Move> moves;
+        return moves;
+    }
 
 
 }
