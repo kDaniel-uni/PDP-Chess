9ef16a865a78ea0af646860b9651e8ae2984269d
Marvin Beites <marvin.beites@etu.u-bordeaux.fr>
2022-04-02 15:51:40 +0200

    add negascout algorithm (tentative)

--- src/src/algorithm/negascout.cpp
+++ src/src/algorithm/negascout.cpp
@@ -6,4 +6,44 @@
 
 namespace pdp_chess {
 
+
+        Move findNextMove(Board& board, color current_color){
+            
+        }
+
+        std::string getParameters(){
+
+        }
+
+        int NegaScout(Board board, int depth, int alpha, int beta, boolean ai_player_turn){
+            if(depth == 0 || board.isGameOver()){
+                return Heuristic::evaluateBoard(board);
+            }
+            int value = std::numeric_limits<int>::min();
+            int return_value = beta;
+            std::vector<Move> legal_moves = legal_moves(board, ai_player_turn);
+            for(Move move : legal_moves){
+                board.doMove(move);
+                int current_value = -NegaScout(board, depth-1, -n, -alpha);
+                if(current_value > value){
+                    if(return_value=beta || d<=2){
+                        value = current_value;
+                        }
+                    else{
+                        value = -NegaScout(board, depth-1, -beta, -current_value);
+                        }
+                    }
+                    if(value > alpha){
+                        alpha = value;
+                        board.undoMove(move);
+                    }
+                    if(alpha >= beta){
+                        return alpha;
+                        n = alpha +1;
+                    }
+                }
+                return value;
+            }
+    };
+
 }
