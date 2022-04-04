cab30f5b734f47e34ccd830c62e6bdb813b3fb5d
Marvin Beites <marvin.beites@etu.u-bordeaux.fr>
2022-04-04 15:46:02 +0200

    add mtdf algorithm (tentative)

--- src/src/algorithm/mtdf.cpp
+++ src/src/algorithm/mtdf.cpp
@@ -6,4 +6,37 @@
 
 namespace pdp_chess {
 
+
+        Move findNextMove(Board& board, color current_color){
+            
+        }
+
+        std::string getParameters(){
+
+        }
+
+        int mtdf(Board board, int f, int depth){
+            int value = f;
+            int beta = 0;
+            int upperBound = std::numeric_limits<int>::max();
+            int lowerBound = std::numeric_limits<int>::min();
+            while(lowerBound < upperBound){
+                if(value = lowerBound){
+                    beta = g + 1:
+                }
+                else{
+                    beta = value;
+                }
+                //value = AlphaBetaWithMemory(board, beta-1, beta, depth);
+                if(value < beta){
+                    upperBound = value;
+                }
+                else{
+                    lowerBound = value;
+                }
+            }
+            return value;
+        }
+    };
+
 }
