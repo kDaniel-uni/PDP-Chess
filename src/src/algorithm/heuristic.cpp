71d67a9413b64f575564aba8d4881d07e98bdf6f
Thomas ZUCCHELLI <user.name>
2022-04-04 14:50:05 +0200

    Completion du calcul de la valeur du board avec les pions arrieres

--- src/src/algorithm/heuristic.cpp
+++ src/src/algorithm/heuristic.cpp
@@ -6,18 +6,85 @@
 #include <heuristic.h>
 
 namespace pdp_chess {
+    Heuristic::Heuristic(){
+        pawns_value = 1;
+        rooks_value = 5;
+        bishops_value = 3;
+        knights_value = 3;
+        queen_value = 9;
+        king_value = 200; 
+        backward_value = 0.5;
+    }
 
-    Heuristic::Heuristic(int p_v, int r_v, int b_v, int kn_v, int q_v, int k_v) {
+    Heuristic::Heuristic(float p_v, float r_v, float b_v, float kn_v, float q_v, float k_v, float back_v) {
         pawns_value = p_v;
         rooks_value = r_v;
         bishops_value = b_v;
         knights_value = kn_v;
         queen_value = q_v;
         king_value = k_v;
+        backward_value = back_v;
     }
 
+    float Heuristic::nbBackward(const Bitboard * bitboard){
+        int cmp = 0;
+        for(int i = 8; i < BOARD_SIZE; i++){
+            if((bitboard->value >> i) & 1) {
+                if((bitboard->value >> i - 8) & 1){
+                    cmp++;
+                }
+            }
+        }
+        return cmp;
+    }
 
-    int Heuristic::evaluateBoard(const Board &board) {
+    float Heuristic::nbDoubled(const Bitboard * bitboard){
+        for(int i = 0; i < BOARD_SIZE; i++){
+            if((bitboard->value >> i) & 1) {
+            }
+        }
         return 0;
+    }
+
+    float Heuristic::nbIsolated(const Bitboard * bitboard){
+        for(int i = 0; i < BOARD_SIZE; i++){
+            if((bitboard->value >> i) & 1) {
+
+            }
+        }
+        return 0;
+    }
+
+    float Heuristic::evaluatePieces(const Bitboards * bitboards){
+        float value = 0;
+        for (int i = 0; i < 6; i++) {
+            Bitboard* b = bitboards->list[i];
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                if ((b->value >> j) & 1) {
+                    if (i == 0)
+                        value += pawns_value;
+                    if (i == 1)
+                        value += rooks_value;
+                    if (i == 2)
+                        value += bishops_value;
+                    if (i == 3)
+                        value += knights_value;
+                    if (i == 4)
+                        value += queen_value;
+                    if (i == 5)
+                        value += king_value;
+                }
+            }
+        }
+        value += backward_value*nbBackward(bitboards->list[0]); 
+        return value;
+    }
+
+    float Heuristic::evaluateBoard(const Board &board, bool white_turn) {
+        float value = evaluatePieces(board._pieces[white]) - evaluatePieces(board._pieces[black]);
+        if (white_turn)
+            return value;
+        else
+            return -1 * value;
     }
 }
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -12,13 +12,16 @@ using namespace pdp_chess;
 int main(int argc, char *argv[])
 {
     Game* g = new Game();
-    Heuristic* h = new Heuristic(1, 5, 3, 3, 9, 200);
+    Heuristic* h = new Heuristic();
     g->drawBoard();
     g->fromString("------------KQ-B-------------------------------------------------");
     g->drawBoard();
-    /*printf("%d\n", g->board.getBoardValue(true, h));*/
-    g->fromString("------------KQ-B----------------------------ppp------------------");
+    printf("%f\n", h->evaluateBoard(g->board, true));
+    g->fromString("------------KQ-B----------------------------ppp---------P------P-");
     g->drawBoard();
-    /*printf("%d\n", g->board.getBoardValue(true, h));*/
+    printf("%f\n", h->evaluateBoard(g->board, true));
+    g->fromString("------------KQ-B----------------------------ppp--------PP------P-");
+    g->drawBoard();
+    printf("%f\n", h->evaluateBoard(g->board, true));
     return argc == 3 ? EXIT_SUCCESS : EXIT_FAILURE; // optional return value
 }
