afdb8762a67c875a7ebc5eb3fc11019f5ed54bbb
Karl Daniel <karl.daniel@etu.u-bordeaux.fr>
2022-04-05 16:56:34 +0200

    previous commit

--- test/legal_move_test.cpp
+++ test/legal_move_test.cpp
@@ -7,203 +7,351 @@
 #include "move.h"
 #include "game.h"
 #include "legal_move.h"
+#include "legal_move_v2.h"
 #include <iostream>
 #include <chrono>
 
 using namespace pdp_chess;
 
-int main (int argc, char *argv[]) {
+bool checkMoveInBasicLegalMoves(const Move &move) {
+    switch (move.start_position) {
+        case a2:
+            if (move.target_position == a3 || move.target_position == a4) {
+                return true;
+            }
+        case b2:
+            if (move.target_position == b3 || move.target_position == b4) {
+                return true;
+            }
+        case c2:
+            if (move.target_position == c3 || move.target_position == c4) {
+                return true;
+            }
+        case d2:
+            if (move.target_position == d3 || move.target_position == d4) {
+                return true;
+            }
+        case e2:
+            if (move.target_position == e3 || move.target_position == e4) {
+                return true;
+            }
+        case f2:
+            if (move.target_position == f3 || move.target_position == f4) {
+                return true;
+            }
+        case g2:
+            if (move.target_position == g3 || move.target_position == g4) {
+                return true;
+            }
+        case h2:
+            if (move.target_position == h3 || move.target_position == h4) {
+                return true;
+            }
+        case b1:
+            if (move.target_position == a3 || move.target_position == c3) {
+                return true;
+            }
+        case g1:
+            if (move.target_position == f3 || move.target_position == h3) {
+                return true;
+            }
+        default :
+            return false;
+    }
+}
+
+bool testPawn(Game &game, bool is_v1, Legalmove &legalmove) {
+    std::vector<Move> to_test;
+    game.board.fromString("--------P--------p----------------------------------------------");
+    game.drawBoard();
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, true);
+    } else {
+        to_test = legalmove.legalMove(game.board, true);
+    }
+
+    if (to_test.size() != 3) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (m.start_position != a2) {
+            return false;
+        } else {
+            if (m.target_position != a4 && m.target_position != a3 && m.target_position != b3) {
+                return false;
+            }
+        }
+    }
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, false);
+    } else {
+        to_test = legalmove.legalMove(game.board, false);
+    }
+
+    if (to_test.size() != 2) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (m.start_position != b3) {
+            return false;
+        } else {
+            if (m.target_position != b2 && m.target_position != a2) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool testRooks(Game &game, bool is_v1, Legalmove &legalmove) {
+    std::vector<Move> to_test;
+    game.board.fromString("R---------------r-----------------------------------------------");
+    game.drawBoard();
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, true);
+    } else {
+        to_test = legalmove.legalMove(game.board, true);
+    }
+
+    if (to_test.size() != 9) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (m.start_position != a1) {
+            return false;
+        } else if (m.target_position != a2 && m.target_position != a3 && m.target_position != b1 &&
+                   m.target_position != c1 && m.target_position != d1 && m.target_position != e1 &&
+                   m.target_position != f1 && m.target_position != g1 && m.target_position != h1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool testBishops(Game &game, bool is_v1, Legalmove &legalmove) {
+    std::vector<Move> to_test;
+    game.board.fromString("---------B--------------------------b---------------------------");
+    game.drawBoard();
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, true);
+    } else {
+        to_test = legalmove.legalMove(game.board, true);
+    }
+
+    if (to_test.size() != 6) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (m.start_position != b2) {
+            return false;
+        } else if (m.target_position != a1 && m.target_position != a3 && m.target_position != c1 &&
+                   m.target_position != c3 && m.target_position != d4 && m.target_position != e5) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool testKnights(Game &game, bool is_v1, Legalmove &legalmove) {
+    std::vector<Move> to_test;
+    game.board.fromString("P----------n-----N----------------------------------------------");
+    game.drawBoard();
+
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, true);
+    } else {
+        to_test = legalmove.legalMove(game.board, true);
+    }
+
+    if (to_test.size() != 6) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (m.start_position != b3 && m.start_position != a1) {
+            return false;
+        } else if (m.target_position != c1 && m.target_position != d2 && m.target_position != d4 &&
+                   m.target_position != a5 && m.target_position != c5 && m.target_position != a2) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool testQueen(Game &game, bool is_v1, Legalmove &legalmove) {
+    std::vector<Move> to_test;
+    game.board.fromString("-----P-Q--------------------q----------p------------------------");
+    game.drawBoard();
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, true);
+    } else {
+        to_test = legalmove.legalMove(game.board, true);
+    }
+
+    if (to_test.size() != 9) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (m.start_position != h1 && m.start_position != f1) {
+            return false;
+        } else if (m.target_position != g1 && m.target_position != g2 && m.target_position != f3 &&
+                   m.target_position != e4 && m.target_position != h2 && m.target_position != h3 &&
+                   m.target_position != h4 && m.target_position != h5 && m.target_position != f2) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool testKing(Game &game, bool is_v1, Legalmove &legalmove) {
+    std::vector<Move> to_test;
+    game.board.fromString("K-------p------p------------------------------------------------");
+    game.drawBoard();
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, true);
+    } else {
+        to_test = legalmove.legalMove(game.board, true);
+    }
+
+    if (to_test.size() != 3) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (m.start_position != a1) {
+            return false;
+        } else if (m.target_position != b1 && m.target_position != a2 && m.target_position != b2) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool testBasic(Game &game, bool is_v1, Legalmove &legalmove) {
+    std::vector<Move> to_test;
+    game.board.fromString("RNBQKBNRPPPPPPPP--------------------------------pppppppprnbqkbnr");
+    game.drawBoard();
+
+    if (is_v1) {
+        to_test = pdp_chess::legal_move(game.board, true);
+    } else {
+        to_test = legalmove.legalMove(game.board, true);
+    }
+
+    if (to_test.size() != 20) {
+        return false;
+    }
+
+    for (Move m : to_test) {
+        if (!checkMoveInBasicLegalMoves(m)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int main(int argc, char *argv[]) {
     std::string arg = argv[1];
     Game g = Game();
-    std::vector<Move> to_test;
+    Legalmove legal_moves = Legalmove();
 
-    if (arg == "1"){
-        g.board.fromString("--------P--------p----------------------------------------------");
-        g.drawBoard();
+    if (arg == "1") {
+        return !testPawn(g, true, legal_moves);
 
-        auto begin = std::chrono::high_resolution_clock::now();
-        uint32_t iterations = 1000000;
-        for(uint32_t i = 0; i < iterations; ++i)
-        {
-            to_test = pdp_chess::legal_move(g.board,true);
+    } else if (arg == "2") {
+        return !testPawn(g, false, legal_moves);
 
-            if (to_test.size() != 3){
-                return EXIT_FAILURE;
-            }
+    } else if (arg == "3") {
+        return !testRooks(g, true, legal_moves);
 
-            for(Move m : to_test){
-                if(m.start_position != a2 ){
-                    return EXIT_FAILURE;
-                }
-                else{
-                    if(m.target_position != a4 && m.target_position != a3 && m.target_position != b3 ){
-                        return EXIT_FAILURE;
-                    }
-                }
-            }
+    } else if (arg == "4") {
+        return !testRooks(g, false, legal_moves);
 
-            to_test = pdp_chess::legal_move(g.board,false);
+    } else if (arg == "5") {
+        return !testBishops(g, true, legal_moves);
 
-            if (to_test.size() != 2){
-                return EXIT_FAILURE;
-            }
+    } else if (arg == "6") {
+        return !testBishops(g, false, legal_moves);
 
-            for(Move m : to_test){
-                if(m.start_position != b3 ){
-                    return EXIT_FAILURE;
-                }
-                else{
-                    if( m.target_position != b2 && m.target_position != a2 ){
-                        return EXIT_FAILURE;
-                    }
-                }
-            }
+    } else if (arg == "7") {
+        return !testKnights(g, true, legal_moves);
+
+    } else if (arg == "8") {
+        return !testKnights(g, false, legal_moves);
+
+    } else if (arg == "9") {
+        return !testQueen(g, true, legal_moves);
+
+    } else if (arg == "10") {
+        return !testQueen(g, false, legal_moves);
+
+    } else if (arg == "11") {
+        return !testKing(g, true, legal_moves);
+
+    } else if (arg == "12") {
+        return !testKing(g, false, legal_moves);
+
+    } else if (arg == "13") {
+        return !testBasic(g, true, legal_moves);
+
+    } else if (arg == "14") {
+        return !testBasic(g, false, legal_moves);
+
+    } else if (arg == "15") {
+        auto t1 = std::chrono::high_resolution_clock::now();
+
+        for (int index = 0; index < 100000; index++){
+            //testPawn(g, true, legal_moves);
+            testRooks(g, true, legal_moves);
+            testBishops(g, true, legal_moves);
+            testKnights(g, true, legal_moves);
+            testQueen(g, true, legal_moves);
+            testKing(g, true, legal_moves);
+            //testBasic(g, true, legal_moves);
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-        std::cout << duration << "ns total, average : " << duration / iterations << "ns." << std::endl;
 
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = duration_cast<std::chrono::milliseconds>(t2 - t1);
+        std::cout << duration.count() << " ms" << std::endl;
         return EXIT_SUCCESS;
 
-    }else if( arg == "2"){
-        g.board.fromString("R---------------r-----------------------------------------------");
-        g.drawBoard();
+    }else if (arg == "16") {
+        auto t1 = std::chrono::high_resolution_clock::now();
 
-        auto begin = std::chrono::high_resolution_clock::now();
-        uint32_t iterations = 1000000;
-        for(uint32_t i = 0; i < iterations; ++i)
-        {
-            to_test = pdp_chess::legal_move(g.board,true);
-
-            if (to_test.size() != 9){
-                return EXIT_FAILURE;
-            }
-
-            for(Move m : to_test){
-                if(m.start_position != a1 ){
-                    return EXIT_FAILURE;
-                }else if( m.target_position != a2 && m.target_position != a3 && m.target_position != b1 && m.target_position != c1 && m.target_position != d1 && m.target_position != e1 && m.target_position != f1 && m.target_position != g1 && m.target_position != h1 ){
-                    return EXIT_FAILURE;
-                }
-            }
+        for (int index = 0; index < 100000; index++){
+            //testPawn(g, false, legal_moves);
+            testRooks(g, false, legal_moves);
+            testBishops(g, false, legal_moves);
+            testKnights(g, false, legal_moves);
+            testQueen(g, false, legal_moves);
+            testKing(g, false, legal_moves);
+            //testBasic(g, false, legal_moves);
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-        std::cout << duration << "ns total, average : " << duration / iterations << "ns." << std::endl;
 
-        return EXIT_SUCCESS;
-    
-    }else if( arg == "3"){
-        g.board.fromString("---------B--------------------------b---------------------------");
-        g.drawBoard();
-
-        auto begin = std::chrono::high_resolution_clock::now();
-        uint32_t iterations = 1000000;
-        for(uint32_t i = 0; i < iterations; ++i)
-        {
-            to_test = pdp_chess::legal_move(g.board,true);
-
-            if (to_test.size() != 6){
-                return EXIT_FAILURE;
-            }
-
-            for(Move m : to_test){
-                if( m.start_position != b2){
-                    return EXIT_FAILURE;
-                }else if( m.target_position != a1 && m.target_position != a3 &&m.target_position != c1 && m.target_position != c3 && m.target_position != d4 && m.target_position != e5 ){
-                    return EXIT_FAILURE;
-                }
-            }
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-        std::cout << duration << "ns total, average : " << duration / iterations << "ns." << std::endl;
-
-        return EXIT_SUCCESS;
-
-    }else if( arg == "4"){
-        g.board.fromString("P----------n-----N----------------------------------------------");
-        g.drawBoard();
-
-        auto begin = std::chrono::high_resolution_clock::now();
-        uint32_t iterations = 1000000;
-        for(uint32_t i = 0; i < iterations; ++i)
-        {
-            to_test = pdp_chess::legal_move(g.board,true);
-
-            if (to_test.size() != 6){
-                return EXIT_FAILURE;
-            }
-
-            for(Move m : to_test){
-                if( m.start_position != b3 && m.start_position != a1){
-                    return EXIT_FAILURE;
-                }else if( m.target_position != c1 && m.target_position != d2 && m.target_position != d4 && m.target_position != a5 && m.target_position != c5 && m.target_position != a2){
-                    return EXIT_FAILURE;
-                }
-            }
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-        std::cout << duration << "ns total, average : " << duration / iterations << "ns." << std::endl;
-
-        return EXIT_SUCCESS;
-
-    }else if( arg == "5"){
-        g.board.fromString("-----P-Q--------------------q----------p------------------------");
-        g.drawBoard();
-
-        auto begin = std::chrono::high_resolution_clock::now();
-        uint32_t iterations = 1000000;
-        for(uint32_t i = 0; i < iterations; ++i)
-        {
-            to_test = pdp_chess::legal_move(g.board,true);
-
-            if (to_test.size() != 9){
-                return EXIT_FAILURE;
-            }
-
-            for( Move m : to_test){
-                if ( m.start_position != h1 && m.start_position != f1){
-                    return EXIT_FAILURE;
-                }else if( m.target_position != g1 && m.target_position != g2 && m.target_position != f3 && m.target_position != e4 && m.target_position != h2 && m.target_position != h3 && m.target_position != h4 && m.target_position != h5 && m.target_position != f2){
-                    return EXIT_FAILURE;
-                }
-            }
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-        std::cout << duration << "ns total, average : " << duration / iterations << "ns." << std::endl;
-
-        return EXIT_SUCCESS;
-
-    }else if( arg == "6"){
-        g.board.fromString("K-------p------p------------------------------------------------");
-        g.drawBoard();
-
-        auto begin = std::chrono::high_resolution_clock::now();
-        uint32_t iterations = 1000000;
-        for(uint32_t i = 0; i < iterations; ++i)
-        {
-            to_test = pdp_chess::legal_move(g.board,true);
-
-            if (to_test.size() != 3){
-                return EXIT_FAILURE;
-            }
-
-            for(Move m : to_test){
-                if (m.start_position != a1){
-                    return EXIT_FAILURE;
-                }else if( m.target_position != b1 && m.target_position != a2 && m.target_position != b2 ){
-                    return EXIT_FAILURE;
-                }
-            }
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-        std::cout << duration << "ns total, average : " << duration / iterations << "ns." << std::endl;
-
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = duration_cast<std::chrono::milliseconds>(t2 - t1);
+        std::cout << duration.count() << " ms" << std::endl;
         return EXIT_SUCCESS;
     }
     return EXIT_FAILURE;
+
 }
