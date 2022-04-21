0333c434927dcc9d0b8eeefbacfb51582873aa17
Karl Daniel <karl.daniel@etu.u-bordeaux.fr>
2022-04-21 18:25:52 +0200

    prepare test for version test

--- src/test/versions_test.cpp
+++ src/test/versions_test.cpp
@@ -15,7 +15,24 @@ int main (int argc, char *argv[]) {
     std::string arg = argv[1];
 
     if (arg == "1"){
-        int number_of_game = 20;
+        int number_of_game = 200;
+
+        Director director = Director();
+        director.LoadMatchFromJson("../json_exemple/ai_test.json", true);
+
+        auto t1 = std::chrono::high_resolution_clock::now();
+
+        for (int i = 0; i < number_of_game; ++i) {
+            director.PlayMatch(false);
+            director.ResetGame();
+        }
+
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = duration_cast<std::chrono::milliseconds>(t2 - t1);
+        std::cout << number_of_game << " game played" << std::endl;
+        std::cout << "Average game time " << duration.count()/number_of_game << " ms" << std::endl;
+    } if (arg == "2"){
+        int number_of_game = 200;
 
         Director director = Director();
         director.LoadMatchFromJson("../json_exemple/ai_test.json", false);
